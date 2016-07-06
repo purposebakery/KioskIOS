f00d391fade48d036da7c4f737f1ee3cccf73bff
Metz, Oliver, NMM-NM <oliver.metz@bertelsmann.de>
2016-07-06 19:55:06 +0200

    xcode project init

---
+++ KioskIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KioskIOS
+//
+//  Created by Metz, Oliver, NMM-NM on 7/6/16.
+//  Copyright © 2016 techlung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
