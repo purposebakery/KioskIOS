24d98f2ff1d0f4f3abdb5c85d1a4314cac078560
Metz, Oliver, NMM-NM <oliver.metz@bertelsmann.de>
2016-07-06 22:44:13 +0200

    More controllers

---
+++ KioskIOS/PaymentViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PaymentViewController.swift
+//  KioskIOS
+//
+//  Created by Metz, Oliver, NMM-NM on 7/6/16.
+//  Copyright © 2016 techlung. All rights reserved.
+//
+
+import UIKit
+
+class PaymentViewController: UIViewController {
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "Payment"
+    }
+}
